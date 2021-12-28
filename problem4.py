c280f122beb0c666b5207c0531b8020a14fb9aa1
andrewkos <andrew_kos@hotmail.com>
2021-12-28 21:34:31 +0000

    Finished problem 4

--- problem4.py
+++ problem4.py
@@ -27,15 +27,56 @@ for i in range(1,len(a)):
         boardBlocks[boardBlockIndex].append(a[i])
 # --------------------------------------------------------------
 
-class board:
+class Board:
     def __init__(self,boardBlock:List[str]):
-            self.numbers = [for someStr in boardBlock :
-                                 spl = str.split(" ")
-                                 list(filter(lambda someStr: someStr != "",spl))]
+            self.numbers = [someStr.split() for someStr in boardBlock]
+
+            self.observed = [[False for _ in range(5)] for _ in range(5)]
+
+            self.hasWon = False
+
+    def observe(self,call:str):
+            for rowIndex,row in enumerate(self.numbers):
+                for elementIndex,element in enumerate(row):
+                    if element == call:
+                        self.observed[rowIndex][elementIndex] = True
+
+    def score(self,lastObserved:str):
+            totalScore = 0
+            for rowIndex,row in enumerate(self.observed):
+                for elementIndex,wasObserved in enumerate(row):
+                    if not(wasObserved):
+                        totalScore += int(self.numbers[rowIndex][elementIndex])
+
+            return totalScore*int(lastObserved)
+
+    def bingo(self):
+
+            for rowTest in range(5):
+                    if self.observed[rowTest][0] == True and self.observed[rowTest][1] == True and self.observed[rowTest][2] == True and self.observed[rowTest][3] == True and self.observed[rowTest][4] == True: 
+                            self.hasWon = True
+
+            for colTest in range(5):
+                    if self.observed[0][colTest] == True and self.observed[1][colTest] == True and self.observed[2][colTest] == True and self.observed[3][colTest] == True and self.observed[4][colTest] == True: 
+                            self.hasWon = True
+
+            return self.hasWon
+
+            
+boards = [Board(bb) for bb in boardBlocks]
 
 
+for c in calls:
+    for b in boards:
+        if b.hasWon == False:
+            b.observe(c)
+            if b.bingo():
+                finished = True
+                print(b.score(c))
+         
 
-print(f"{boardBlocks}")
+
+#print(f"{boardBlocks}")
 
 if __name__ == "__main__":
     print("I was called")
