ef02ff5998f2a3faf0bf6d8be190038ce63738b3
andrewkos <andrew_kos@hotmail.com>
2022-01-04 22:08:37 +0000

    prob 10

--- problem10.py
+++ problem10.py
@@ -14,10 +14,70 @@ def read_text(name="input.txt"):
     return text_file.read_text()
 
 
+pairs = {"(" : ")",
+         "<" : ">",
+         "[" : "]",
+         "{" : "}"}
+
+scores = {")": 3,
+          "]": 57,
+          "}": 1197,
+          ">": 25137}
+
 a = read_text("Input10.txt").splitlines()
 
 class chunk():
-    def __init__(self,message,parent):
-        self.symbol = message[0]
-        self.parent = parent
+    def __init__(self):
+        self.open_symbol = None
+        self.close_symbol = None
+        self.parent = None
+        self.children = []
+
+    def check(self):
+        score = 0
+        #print(f"Check {self.open_symbol}{self.close_symbol}")
+        if (self.close_symbol != pairs[self.open_symbol]):
+            if not(self.close_symbol is None):
+                 #print(f"Mismatch {self.open_symbol}{self.close_symbol}")
+                 score += scores[self.close_symbol]
+
+        return score
+
+    def check_self_and_children(self):
+        score = self.check()
+        if len(self.children) > 0:
+            for child in self.children:
+                if score == 0:
+                        #print("here")
+                        score += child.check_self_and_children()
+
+        return score
+
+score = 0
+
+for str in a:
+
+    start_chunk = chunk()
+    start_chunk.open_symbol = str[0]
+    #print(f"{start_chunk.open_symbol}")
+    working_chunk = start_chunk
+    for element in str[1::]:
+        if pairs.keys().__contains__(element):
+            new_chunk = chunk()
+            new_chunk.open_symbol = element
+            #print(f"New chunk = {new_chunk.open_symbol}")
+            new_chunk.parent = working_chunk
+            working_chunk.children.append(new_chunk)
+            working_chunk = new_chunk
+        else:
+            working_chunk.close_symbol = element
+            #print(f"Close chunk = {new_chunk.close_symbol}")
+            #print(f"{working_chunk.open_symbol}{working_chunk.close_symbol}")
+            working_chunk = working_chunk.parent
+            
+
+    score += start_chunk.check_self_and_children()
+
+print(f"{score}")
+
         
